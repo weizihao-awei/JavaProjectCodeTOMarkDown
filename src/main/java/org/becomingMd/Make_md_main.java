7d6b87f0820421cc9e2a893d71931faa39eb976e
86158 <1586839217@qq.com>
2025-03-20 21:20:18 +0800

    feat:增加导出模式选择功能
    - 新增导出模式选择功能，用户可以选择默认模式或导出三级 Java
    - 默认模式：保留原有逻辑，将所有文件内容复制到 MD 文件中
    - 三级 Java 模式：仅将源码文件复制到 MD 文件中，每个文件放在一个三级标题内
    - 优化了用户提示信息，增加了对路径输入的注意事项说明

--- src/main/java/org/becomingMd/Make_md_main.java
+++ src/main/java/org/becomingMd/Make_md_main.java
@@ -29,12 +29,17 @@ public class Make_md_main {
         //D:\专业学习资料\Java学习\Java_项目\TakeNoteObsidian
         //C:\Users\86158\Desktop\ob_整理文件测试\text
 
-        //
         Scanner scanner = new Scanner(System.in);
-        System.out.println("请输入源码软件包的绝对路径：");
+        System.out.println("请输入源码软件包的绝对路径（注意地址不要带双引号）：");
         String pathString = scanner.nextLine();
-        System.out.println("请输入要生成的md文件存放的文件夹的绝对地址:");
+        System.out.println("请输入要生成的md文件存放的文件夹的绝对地址（注意地址不要带双引号）:");
         String targetPath = scanner.nextLine();
+
+        System.out.println("请选择要导出的模式：");
+        System.out.println("1.默认模式");
+        System.out.println("2.导出三级java");
+        System.out.println("请输入1或者2后回车：");
+        int mode = scanner.nextInt();
 
         //读取软件包生成md文档
         File dir = new File(pathString);
@@ -42,14 +47,51 @@ public class Make_md_main {
         File target = new File(targetPath+"\\"+mdName);
             //获取软件包处在绝对路径上的层级
         int InitialLevel =FileLayerExample.getFileLayer(dir);
-        List<String> md = getAllFileContents(dir,InitialLevel);
+        List<String> md = getAllFileContents(dir,InitialLevel,mode);
         Copy_file.copy_file(md,target);
     }
 
+    /**
+     *
+     * @param dir 要获取源码的软件包文件夹的绝对路径地址：D:\专业学习资料\Java学习\Java_项目\TakeNoteObsidian
+     * @param InitialLevel 源码软件包在绝对路径上的层级，例如：TakeNoteObsidian的层级为3，在三个文件夹内
+     * @return md ：保存这个软件包所有源码文件的内容，以List<String> md 形式。
+     *
+     */
 
-    public static List<String> getAllFileContents(File dir,int InitialLevel) throws IOException {
+    public static List<String> getAllFileContents(File dir,int InitialLevel,int mode) throws IOException {
+        List<String> md = null;
+        //选择模式
+        switch (mode) {
+            case 1:
+                // 标准默认模式
+                md=DefaultMode(dir,InitialLevel);
+                break;
+            case 2:
+                // 模式2的操作
+                md=threeTitleCode(dir,InitialLevel);
+
+                break;
+            default:
+                System.out.println("无效的模式选择");
+                break;
+        }
+
+        return md;
+    }
+
+    /**
+     * 默认的生成md模式，使用文件名作为标题，不仅是把源代码复制到md内，同时会复制其它文件的文件路径到md内
+     *
+     *  @param dir 要获取源码的软件包文件夹的绝对路径地址：D:\专业学习资料\Java学习\Java_项目\TakeNoteObsidian
+     *  @param InitialLevel 源码软件包在绝对路径上的层级，例如：TakeNoteObsidian的层级为3，在三个文件夹内
+     *  @return md ：保存这个软件包所有源码文件的内容，以List<String> md 形式。
+     */
+    public static List<String> DefaultMode(File dir,int InitialLevel) throws IOException {
         File[] files = dir.listFiles();
-        List<String> md = new ArrayList<>();
+        List<String> md= new ArrayList<>();
+
+
         //list保存没有匹配到后缀名的文件的绝对路径
         List<String> listFilepath = new ArrayList<>();
         //确定软件包的层级
@@ -64,14 +106,14 @@ public class Make_md_main {
                 md.add(getTitle(titleLevel,f.getName()));
                 md.add("\n");
                 // 递归获取子目录文件路径，并合并结果
-                md.addAll(getAllFileContents(f,InitialLevel)) ;
+                md.addAll(DefaultMode(f,InitialLevel)) ;
             } else {
 
-
+                // md file
 
                 // 确定标题的层级
-                 fileLayer = FileLayerExample.getFileLayer(f);
-                 titleLevel = fileLayer - InitialLevel;
+                fileLayer = FileLayerExample.getFileLayer(f);
+                titleLevel = fileLayer - InitialLevel;
                 // 将文件名作为标题添加到md中
 
                 if(!Permitted_suffix_names.contains(getFileSuffixName(f.getName()))){
@@ -89,11 +131,6 @@ public class Make_md_main {
 
                 }
 
-
-
-
-
-
             }
         }
         if(!listFilepath.isEmpty()){
@@ -103,9 +140,47 @@ public class Make_md_main {
             md.add("\n");
             md.addAll(listFilepath);
         }
+        return md;
+    }
+
+    /**
+     * 给出一个文件夹，只将里面的源码文件复制到md内，并返回md，
+     * 在生成的md文件，每一个源码文件放在一个三级标题内，而三级标题名字就是对应源码文件的名字。
+     *
+     *  @param dir 要获取源码的软件包文件夹的绝对路径地址：D:\专业学习资料\Java学习\Java_项目\TakeNoteObsidian
+     *  @param InitialLevel 源码软件包在绝对路径上的层级，例如：TakeNoteObsidian的层级为3，在三个文件夹内
+     *  @return md ：保存这个软件包所有源码文件的内容，以List<String> md 形式。
+     */
+    public static List<String> threeTitleCode(File dir,int InitialLevel)throws IOException{
+        File[] files = dir.listFiles();
+        List<String> md= new ArrayList<>();
+
+        for (File f : files) {
+            if (f.isDirectory()) {
+                // 递归获取子目录文件路径，并合并结果
+                md.addAll(threeTitleCode(f,InitialLevel)) ;
+            } else {
+                // 将文件名作为标题添加到md中
+                if(Permitted_suffix_names.contains(getFileSuffixName(f.getName()))){
+                    md.add("### "+f.getName());
+                    md.add("\n");
+                    //获取文件后缀名
+                    md.add("```"+getFileSuffixName(f.getName()));
+                    List<String> fileContent = Read_File.read_md(f);
+                    md.addAll(fileContent);
+                    md.add("```");
+                    md.add("\n");
+
+                }
+
+
+
+            }
+        }
 
         return md;
     }
+
     public static String getTitle(int layer,String fileName){
 
             StringBuilder title = new StringBuilder("#");
@@ -115,9 +190,9 @@ public class Make_md_main {
             //删掉文件名后缀
             if(fileName.contains(".")){
                 fileName = fileName.substring(0, fileName.lastIndexOf("."));
-                title.append(" ").append(fileName).append("类");
+                title.append(" ").append(fileName);
             }else{
-                title.append(" ").append(fileName).append("软件包");
+                title.append(" ").append(fileName);
             }
 
             return title.toString();
